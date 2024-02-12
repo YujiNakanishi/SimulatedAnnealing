662af27fb510ef3105a68260f8ba66a74879c0f9
nakanishi <syghabc@gmail.com>
2024-02-12 15:49:29 +0900

    done test1.py

--- test1.py
+++ test1.py
@@ -10,4 +10,12 @@ objective_function = function
 sampler = Gauss()
 x_init = np.zeros(2)
 
-T = SAT.util.decideInitialTemperature(objective_function, sampler, x_init)
+T_init = SAT.util.decideInitialTemperature(objective_function, sampler, x_init, sample_num = 100)
+T_scheduler = SAT.ECS(T_init)
+
+module = SA.Module(x_init, sampler, T_scheduler, objective_function)
+module.step(1000)
+
+x_opt, f_opt = module.opt_solution()
+print(x_opt)
+print(f_opt)
